a9508cf68824f7928de2647373aef9447a3d8369
Degenerative Art <zaidudev@gmail.com>
2022-01-07 12:20:22 +0300

    Initial contract proof of concept
    With tests, bells and whistles

---
+++ contracts/DApes.sol
@@ -0,0 +1,65 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DApes is ERC721Enumerable, Ownable {
+    struct CollectionIndices {
+        uint256 next;
+        uint256 end;
+    }
+    
+    string private baseURIprefix;
+    address public gatekeeper;
+    mapping(bytes32 => bool) public usedKeys;
+    CollectionIndices[] public collectionIndices;
+
+    constructor(address aGatekeeper) ERC721("DApes", "DAPES") {
+        gatekeeper = aGatekeeper;
+    }
+
+    function setGatekeeper(address aGatekeeper) public onlyOwner {
+        gatekeeper = aGatekeeper;
+    }
+
+    function isKeyHashValid(bytes32 message, bytes memory signature) internal view returns (bool) {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(signature);
+        return ecrecover(message, v, r, s) == gatekeeper;
+    }
+
+    function splitSignature(bytes memory sig) internal pure returns(bytes32 r, bytes32 s, uint8 v) {
+        require(sig.length == 65, "Invalid signature");
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+    }
+
+    function addCollection(uint256 start, uint256 end) public onlyOwner {
+        collectionIndices.push(CollectionIndices({ next: start, end: end }));
+    }
+
+    function keyHash(uint256 collection, uint256 nonce, address addr) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n84", collection, nonce, addr));
+    }
+
+    function mint(uint256 collection, uint256 nonce, bytes memory signature) public {
+        bytes32 kh = keyHash(collection, nonce, msg.sender);
+        require(isKeyHashValid(kh, signature), "Invalid access key");
+        require(!usedKeys[kh], "Key already used");
+        require(collectionIndices[collection].next < collectionIndices[collection].end, "Minted out");
+
+        usedKeys[kh] = true;
+        _safeMint(msg.sender, collectionIndices[collection].next++);
+    }
+
+    function setBaseURI(string calldata newURI) public onlyOwner {
+        baseURIprefix = newURI;
+    }
+
+    function _baseURI() override internal view returns(string memory) {
+        return baseURIprefix;
+    }
+}
---
+++ test/index.ts
@@ -0,0 +1,88 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { DApes, DApes__factory } from "../typechain";
+
+function padZeros(s: string, width: number) {
+  const padSize = width - s.length;
+  if (padSize === 0) {
+    return s;
+  } else {
+    return "0".repeat(padSize) + s;
+  }
+}
+
+function packKey(collection: number, nonce: number, address: string) {
+  return "0x" + padZeros(collection.toString(16), 64) + padZeros(nonce.toString(16), 64) + padZeros(address.slice(2), 40);
+}
+
+describe("DApes", function () {
+  let DApes: DApes__factory;
+  let dapesContract: DApes;
+
+  let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let addr2: SignerWithAddress;
+  let gatekeeper: SignerWithAddress;
+  let addrs: SignerWithAddress[];
+  beforeEach(async function() {
+    DApes = await ethers.getContractFactory("DApes");
+    [owner, addr1, addr2, gatekeeper, ...addrs] = await ethers.getSigners();
+    dapesContract = await DApes.deploy(gatekeeper.address);
+  })
+
+  async function sign(collection:number, nonce:number, addr:string) {
+    return await gatekeeper.signMessage(ethers.utils.arrayify(packKey(collection, nonce, addr)));
+  }
+
+  it("Can mint with proper signed key", async function () {
+    await dapesContract.addCollection(0, 10);
+    let indices = await dapesContract.collectionIndices(0);
+    expect(indices[0]).to.equal(0);
+    expect(indices[1]).to.equal(10);
+
+    await expect(dapesContract.connect(addr1).mint(0, 0, await gatekeeper.signMessage(ethers.utils.arrayify(packKey(0, 0, addr1.address)))))
+      .to.be.not.reverted;
+  });
+
+  it("Cannot be minted with stolen key", async function() {
+    await dapesContract.addCollection(0, 10);
+  
+    const stolen_sig = await gatekeeper.signMessage(ethers.utils.arrayify(packKey(0, 0, addr1.address)));
+    await expect(dapesContract.connect(addr2).mint(0, 0, stolen_sig)).to.be.revertedWith("Invalid access key");  
+  });
+
+  it("Can mint with several keys", async function () {
+    await dapesContract.addCollection(0, 10);
+    
+    let nonce = 0;
+    await expect(dapesContract.connect(addr1).mint(0, nonce, await gatekeeper.signMessage(ethers.utils.arrayify(packKey(0, nonce, addr1.address))))).to.be.not.reverted;
+    nonce++;
+    await expect(dapesContract.connect(addr1).mint(0, nonce, await gatekeeper.signMessage(ethers.utils.arrayify(packKey(0, nonce, addr1.address))))).to.be.not.reverted;
+    nonce++
+    
+    await expect(dapesContract.connect(addr2).mint(0, nonce, await gatekeeper.signMessage(ethers.utils.arrayify(packKey(0, nonce, addr2.address))))).to.be.not.reverted;
+    await expect(dapesContract.connect(addr2).mint(0, nonce, await gatekeeper.signMessage(ethers.utils.arrayify(packKey(0, nonce, addr2.address))))).to.be.revertedWith("Key already used");
+
+    while (nonce < 9) {
+      await expect(dapesContract.connect(addr1).mint(0, nonce, await gatekeeper.signMessage(ethers.utils.arrayify(packKey(0, nonce, addr1.address))))).to.be.not.reverted;
+      nonce++;
+    }
+    await expect(dapesContract.connect(addr1).mint(0, nonce, await sign(0, nonce, addr1.address))).to.be.revertedWith("Minted out");
+    expect(await dapesContract.totalSupply()).to.equal(10);
+  });
+
+  it("Can mint from two collections at the same time", async () => {
+    await dapesContract.addCollection(0, 5);
+    await dapesContract.addCollection(1000, 10002);
+
+    let nonce = 0;
+    let dapes1 = dapesContract.connect(addr1);
+    await expect(dapes1.mint(0, nonce, await sign(0, nonce, addr1.address))).to.be.not.reverted;
+    nonce++;
+    await expect(dapes1.mint(1, nonce, await sign(1, nonce, addr1.address))).to.be.not.reverted;
+    nonce++;
+    expect(await dapesContract.tokenOfOwnerByIndex(addr1.address, 0)).to.equal(0);
+    expect(await dapesContract.tokenOfOwnerByIndex(addr1.address, 1)).to.equal(1000);
+  });
+});

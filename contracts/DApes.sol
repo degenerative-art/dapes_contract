fa3db630308956ce6cd432a54e6fb61015297b82
Degenerative Art <zaidudev@gmail.com>
2022-01-07 15:33:32 +0300

    Use ECDSA lib for sig recover

--- contracts/DApes.sol
+++ contracts/DApes.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract DApes is ERC721Enumerable, Ownable {
@@ -26,32 +27,18 @@ contract DApes is ERC721Enumerable, Ownable {
         gatekeeper = aGatekeeper;
     }
 
-    function isKeyHashValid(bytes32 message, bytes memory signature) internal view returns (bool) {
-        (bytes32 r, bytes32 s, uint8 v) = splitSignature(signature);
-        return ecrecover(message, v, r, s) == gatekeeper;
-    }
-
-    function splitSignature(bytes memory sig) internal pure returns(bytes32 r, bytes32 s, uint8 v) {
-        require(sig.length == 65, "Invalid signature");
-        assembly {
-            r := mload(add(sig, 32))
-            s := mload(add(sig, 64))
-            v := byte(0, mload(add(sig, 96)))
-        }
-    }
-
     function addCollection(uint256 start, uint256 end) public onlyOwner {
         require(collectionIndices.length == 0 || collectionIndices[collectionIndices.length - 1].end <= start, "Collections shouldn't overlap");
         collectionIndices.push(CollectionIndices({ next: Counters.Counter(start), end: end }));
     }
 
     function keyHash(uint256 collection, uint256 nonce, address addr) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n84", collection, nonce, addr));
+        return ECDSA.toEthSignedMessageHash(abi.encodePacked(collection, nonce, addr));
     }
 
     function mint(uint256 collection, uint256 nonce, bytes memory signature) public {
         bytes32 kh = keyHash(collection, nonce, msg.sender);
-        require(isKeyHashValid(kh, signature), "Invalid access key");
+        require(ECDSA.recover(kh, signature) == gatekeeper, "Invalid access key");
         require(!usedKeys[kh], "Key already used");
         require(collectionIndices[collection].next.current() < collectionIndices[collection].end, "Minted out");
 
